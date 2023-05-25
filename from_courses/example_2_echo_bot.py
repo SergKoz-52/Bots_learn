1e8ce2dd32cf46ac6125f938b661255c8e01e0ec
KozyrevSP <>
2023-05-25 10:01:17 +0300

    изменено окружение

--- from_courses/example_2_echo_bot.py
+++ from_courses/example_2_echo_bot.py
@@ -1,9 +1,6 @@
-import asyncio
-
-from aiogram import Bot, Dispatcher
-from aiogram.dispatcher.filters import Command
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
 from aiogram.types import Message
-
 from src.conf import GenConf
 
 bot_conf = GenConf('spk1bot')
@@ -11,28 +8,38 @@ API_TOKEN: str = bot_conf.token
 
 # Создаем объекты бота и диспетчера
 bot: Bot = Bot(token=API_TOKEN)
-dp: Dispatcher = Dispatcher(bot)
+dp: Dispatcher = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message_handler(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message_handler(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message_handler()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
 
+# Регистрируем хэндлеры
+# можно использовать декораторы, например
+# @dp.message(Command(commands=["start"]))
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_echo)
+
 if __name__ == '__main__':
-    asyncio.run(dp.start_polling())
+    dp.run_polling(bot)
